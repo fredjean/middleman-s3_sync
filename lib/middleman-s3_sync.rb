c012e48686c05568f3c877396ce79714504cd7a7
Frederic Jean <fred@fredjean.net>
2012-12-09 11:28:52 -0700

    Provide feedback to the user.

--- lib/middleman-s3_sync.rb
+++ lib/middleman-s3_sync.rb
@@ -13,31 +13,41 @@ module Middleman
   module S3Sync
     class << self
       def sync
-        puts "Determine which files to upload..."
+        puts "Gathering local files."
         local_files = Dir[options.public_path + "/**/*"]
           .reject { |f| File.directory?(f) }
           .map { |f| f.gsub(/^build\//, '') }
+        puts "Gathering remote files."
         remote_files = bucket.files.map { |f| f.key }
 
         # First pass on the set of files to work with.
+        puts "Determine files to add to S3."
         files_to_push = local_files - remote_files
-        files_to_delete = remote_files - local_files
+        if options.delete
+          puts "Determine which files to delete from S3"
+          files_to_delete = remote_files - local_files
+        end
         files_to_evaluate = local_files - files_to_push
 
         # No need to evaluate the files that are newer on S3 than the local files.
+        puts "Determine which local files are newer than it's counterpart on S3"
         files_to_evaluate.reject! do |f|
+          print '.'
           local_mtime = File.mtime("build/#{f}")
           remote_mtime = s3_files.get(f).last_modified
           remote_mtime >= local_mtime
         end
 
         # Are the files different? Use MD5 to see
+        puts "\n\nDetermine which remaining files are actually different than their S3 counterpart."
         files_to_evaluate.each do |f|
+          print '.'
           local_md5 = Digest::MD5.hexdigest(File.read("build/#{f}"))
           remote_md5 = s3_files.get(f).etag
           files_to_push << f if local_md5 != remote_md5
         end
 
+        puts "\n\nReady to apply updates to S3."
         files_to_push.each do |f|
           if remote_files.include?(f)
             puts "Updating #{f}"
