cd4488c8022394e121b42d59f77f31e9ff1f7f68
Frederic Jean <fred@fredjean.net>
2013-05-16 09:11:55 -0600

    Adding verbosity to the resource creation method (if requested)

--- lib/middleman/s3_sync/resource.rb
+++ lib/middleman/s3_sync/resource.rb
@@ -82,6 +82,12 @@ module Middleman
 
       def create!
         say_status "Creating".green + " #{path}#{ gzipped ? ' (gzipped)'.white : ''}"
+        if options.verbose
+          say_status "Original:    #{original_path.white}"
+          say_status "Local Path:  #{local_path.white}"
+          say_status "remote md5:  #{remote_md5.white}"
+          say_status "content md5: #{content_md5.white}"
+        end
         bucket.files.create(to_h)
       end
 
--- lib/middleman/s3_sync/status.rb
+++ lib/middleman/s3_sync/status.rb
@@ -4,7 +4,7 @@ module Middleman
   module S3Sync
     module Status
       def say_status(status)
-        puts :s3_sync.to_s.rjust(12).green + "  #{status}"
+        puts :s3_sync.to_s.rjust(12).light_green + "  #{status}"
       end
     end
   end
