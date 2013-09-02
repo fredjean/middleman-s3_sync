1a337c0d2b224f3821d3923cb1b9aab2b50e8fec
Frederic Jean <fred@fredjean.net>
2013-09-02 10:35:59 -0600

    Forced updates should honor the ignored flag.
    Completes work on #19

--- lib/middleman/s3_sync.rb
+++ lib/middleman/s3_sync.rb
@@ -120,7 +120,7 @@ module Middleman
       end
 
       def files_to_update
-        return resources.select { |r| r.local? } if s3_sync_options.force
+        return resources.select { |r| r.local? && !r.to_ignore? } if s3_sync_options.force
 
         @files_to_update ||= resources.select { |r| r.to_update? }
       end
