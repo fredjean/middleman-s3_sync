0fa14f59edea32fd2142acd4ef508dfbddefbae6
Johnny Shields <johnny.shields@gmail.com>
2016-04-03 01:02:32 +0900

    RSpec: Use "expect" syntax instead of "should"

--- spec/caching_policy_spec.rb
+++ spec/caching_policy_spec.rb
@@ -7,59 +7,59 @@ describe Middleman::S3Sync::BrowserCachePolicy do
     subject(:policy) { Middleman::S3Sync::BrowserCachePolicy.new(options) }
 
     it "should be blank" do
-      policy.should_not be_nil
+      expect(policy).to_not eq nil
 
-      policy.to_s.should_not =~ /max-age=/
-      policy.to_s.should_not =~ /s-maxage=/
-      policy.to_s.should_not =~ /public/
-      policy.to_s.should_not =~ /private/
-      policy.to_s.should_not =~ /no-cache/
-      policy.to_s.should_not =~ /no-store/
-      policy.to_s.should_not =~ /must-revalidate/
-      policy.to_s.should_not =~ /proxy-revalidate/
-      policy.expires.should be_nil
+      expect(policy.to_s).to_not match /max-age=/
+      expect(policy.to_s).to_not match /s-maxage=/
+      expect(policy.to_s).to_not match /public/
+      expect(policy.to_s).to_not match /private/
+      expect(policy.to_s).to_not match /no-cache/
+      expect(policy.to_s).to_not match /no-store/
+      expect(policy.to_s).to_not match /must-revalidate/
+      expect(policy.to_s).to_not match /proxy-revalidate/
+      expect(policy.expires).to eq nil
     end
 
     context "setting max-age" do
       let(:options) { { max_age: 300 } }
 
-      its(:to_s) { should =~ /max-age=300/ }
+      its(:to_s) { is_expected.to match /max-age=300/ }
     end
 
     context "setting s-maxage" do
       let(:options) { { s_maxage: 300 } }
 
-      its(:to_s) { should =~ /s-maxage=300/ }
+      its(:to_s) { is_expected.to match /s-maxage=300/ }
     end
 
     context "set public flag" do
       let(:options) { { public: true } }
-      its(:to_s) { should =~ /public/ }
+      its(:to_s) { is_expected.to match /public/ }
     end
 
     context "it should set the private flag if it is set to true" do
       let(:options) { { private: true } }
-      its(:to_s) { should =~ /private/ }
+      its(:to_s) { is_expected.to match /private/ }
     end
 
     context "it should set the no-cache flag when set property" do
       let(:options) { { no_cache: true }}
-      its(:to_s) { should =~ /no-cache/ }
+      its(:to_s) { is_expected.to match /no-cache/ }
     end
 
     context "setting the no-store flag" do
       let(:options) { { no_store: true } }
-      its(:to_s) { should =~ /no-store/ }
+      its(:to_s) { is_expected.to match /no-store/ }
     end
 
     context "setting the must-revalidate policy" do
       let(:options) { { must_revalidate: true } }
-      its(:to_s) { should =~ /must-revalidate/ }
+      its(:to_s) { is_expected.to match /must-revalidate/ }
     end
 
     context "setting the proxy-revalidate policy" do
       let(:options) { { proxy_revalidate: true } }
-      its(:to_s) { should =~ /proxy-revalidate/ }
+      its(:to_s) { is_expected.to match /proxy-revalidate/ }
     end
 
     context "divide caching policiies with a comma and a space" do
@@ -67,16 +67,16 @@ describe Middleman::S3Sync::BrowserCachePolicy do
 
       it "splits policies eith commans and spaces" do
         policies = policy.to_s.split(/, /)
-        policies.length.should == 2
-        policies.first.should == 'max-age=300'
-        policies.last.should == 'public'
+        expect(policies.length).to eq 2
+        expect(policies.first).to eq 'max-age=300'
+        expect(policies.last).to eq 'public'
       end
     end
 
     context "set the expiration date" do
       let(:options) { { expires: 1.years.from_now } }
 
-      its(:expires) { should == CGI.rfc1123_date(1.year.from_now )}
+      its(:expires) { is_expected.to eq CGI.rfc1123_date(1.year.from_now )}
     end
   end
 end
@@ -92,7 +92,7 @@ describe "Storing and retrieving policies" do
   it "finds the policies by the mime-type excluding the parameters" do
     caching_policy.add_caching_policy("text/html", max_age: 300)
 
-    expect(policy).to_not be_nil
+    expect(policy).to_not eq nil
     expect(policy.policies.max_age).to eq(300)
   end
 end
@@ -107,14 +107,14 @@ describe "Handling situations where the content type is nil" do
   it "returns the default caching policy when the content type is nil" do
     caching_policy.add_caching_policy(:default, max_age:(60 * 60 * 24 * 365))
 
-    expect(caching_policy.caching_policy_for(nil)).to_not be_nil
+    expect(caching_policy.caching_policy_for(nil)).to_not eq nil
     expect(caching_policy.caching_policy_for(nil).policies[:max_age]).to eq(60 * 60 * 24 * 365)
   end
 
   it "returns the default caching policy when the content type is blank" do
     caching_policy.add_caching_policy(:default, max_age:(60 * 60 * 24 * 365))
 
-    expect(caching_policy.caching_policy_for("")).to_not be_nil
+    expect(caching_policy.caching_policy_for("")).to_not eq nil
     expect(caching_policy.caching_policy_for("").policies[:max_age]).to eq(60 * 60 * 24 * 365)
   end
 end
