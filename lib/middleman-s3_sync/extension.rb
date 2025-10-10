2285525fdc9e5b02f17d466452446cc1b743c436
Frederic Jean <fred@fredjean.net>
2025-10-09 18:29:23 -0600

    Fix: Convert ConfigurationManager to Options object
    The s3_sync_options method was returning Middleman's ConfigurationManager
    directly, which doesn't have our custom acl_enabled? method. Now properly
    converts it to our Options class instance by copying all settings.
    
    This fixes the 'undefined method acl_enabled?' error during deployment.

--- lib/middleman-s3_sync/extension.rb
+++ lib/middleman-s3_sync/extension.rb
@@ -77,7 +77,15 @@ module Middleman
     end
 
     def s3_sync_options
-      options
+      # Convert Middleman's ConfigurationManager to our custom Options object
+      @s3_sync_options_obj ||= begin
+        opts = ::Middleman::S3Sync::Options.new
+        # Copy all option values from Middleman's ConfigurationManager to our Options
+        options.all_settings.each do |key, _|
+          opts.send("#{key}=", options[key]) if opts.respond_to?("#{key}=")
+        end
+        opts
+      end
     end
 
     # Read config options from an IO stream and set them on `self`. Defaults
